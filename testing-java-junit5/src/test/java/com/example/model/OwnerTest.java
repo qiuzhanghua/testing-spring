66ad9a5afb7179beac5e038512c5c5cc2615be9f
qiuzhanghua <qiuzhanghua@icloud.com>
2023-03-31 07:56:39 +0800

    parameterized EnumSource

--- testing-java-junit5/src/test/java/com/example/model/OwnerTest.java
+++ testing-java-junit5/src/test/java/com/example/model/OwnerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -50,6 +51,13 @@ class OwnerTest {
     @ValueSource(strings = {"Spring", "Framework", "Boot"})
     void testValueSource(String val)  {
         System.out.println(val);
+    }
+
+    @DisplayName("Enum Source Test -")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @EnumSource(OwnerType.class)
+    void testEnumSource(OwnerType ownerType)  {
+        System.out.println(ownerType);
     }
 
 }
