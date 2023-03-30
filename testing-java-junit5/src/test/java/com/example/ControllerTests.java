640a82182cf2ed5ee53534ae892a889f99160fdd
qiuzhanghua <qiuzhanghua@icloud.com>
2023-03-30 19:06:53 +0800

    junit default test

--- testing-java-junit5/src/test/java/com/example/ControllerTests.java
+++ testing-java-junit5/src/test/java/com/example/ControllerTests.java
@@ -1,7 +1,14 @@
 package com.example;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Tag("controller")
 public interface ControllerTests {
+    @BeforeAll
+    default void beforeAll() {
+        System.out.println("Lets do something here");
+    }
 }
