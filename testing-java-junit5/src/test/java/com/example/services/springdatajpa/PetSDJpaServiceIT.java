050f29c6226673bfe6bb2e2fbf85fd826640de7d
qiuzhanghua <qiuzhanghua@icloud.com>
2023-03-31 08:28:06 +0800

    Integration Test

---
+++ testing-java-junit5/src/test/java/com/example/services/springdatajpa/PetSDJpaServiceIT.java
@@ -0,0 +1,33 @@
+package com.example.services.springdatajpa;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PetSDJpaServiceIT {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
