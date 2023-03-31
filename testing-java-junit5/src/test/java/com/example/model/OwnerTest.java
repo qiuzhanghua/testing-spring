6506f6f8b3e1bc65919634546cd086a28c925509
qiuzhanghua <qiuzhanghua@icloud.com>
2023-03-31 08:10:00 +0800

    parameterized CsvFileSource

--- testing-java-junit5/src/test/java/com/example/model/OwnerTest.java
+++ testing-java-junit5/src/test/java/com/example/model/OwnerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -69,6 +70,13 @@ class OwnerTest {
             "MI, 3, 3"
     })
     void csvInputTest(String stateName, int val1, int val2)  {
+        System.out.println(stateName + " = " + val1 + " : " + val2);
+    }
+
+    @DisplayName("CSV File Source Test -")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
+    void csvFileInputTest(String stateName, int val1, int val2)  {
         System.out.println(stateName + " = " + val1 + " : " + val2);
     }
 }
